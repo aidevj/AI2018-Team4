fc26d4c08a00a8ae303fc83e4a09015fd674ef3a
Gmyth <332087251@qq.com>
2018-09-18 18:18:40 -0400

    portal add, rescale the terrian
    left problem, carema under the terrian in some case

---
+++ Assets/Teleport.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+	public Transform player;
+
+	public Transform reciever;
+
+	private bool playerIsOverlapping = false;
+	// Update is called once per frame
+	void Update () {
+		if (playerIsOverlapping)
+		{
+			Vector3 portalToPlayer = player.position - transform.position;
+			float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+			if (dotProduct < 0f)
+			{
+				float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
+				rotationDiff += 0;
+				player.Rotate(Vector3.up,rotationDiff);
+
+				Vector3 postionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+				player.position = reciever.position + postionOffset;
+				playerIsOverlapping = false;
+			}
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			playerIsOverlapping = true;
+		}
+	}
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			playerIsOverlapping = false;
+		}
+	}
+}
